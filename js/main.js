c7397d289705574aebd8a8deb80c268e2f2e0f5f
Louis Zhang <Louisgeemod@gmail.com>
2022-11-18 06:02:12 +0000

    user-can-see-list-search-results

--- js/main.js
+++ js/main.js
@@ -6,7 +6,11 @@ var $body = document.querySelector('body');
 var $backHome = document.querySelector('#home');
 var $mobileSearchSubmit = document.querySelector('.position-relative-mobile');
 var $desktopSearchSubmit = document.querySelector('.position-relative-desktop');
+var $mobileSearchBox = document.querySelector('#mobile-search-box');
+var $desktopSearchBox = document.querySelector('#desktop-search-box');
 var $searchResult = document.querySelector('.park-list-row');
+var $parkList = document.querySelector('.park-list');
+var $searchPlaceholder = document.querySelector('.search-placeholder');
 
 var $parkName = document.querySelector('#park-name');
 var $parkImages1 = document.querySelector('#park-images1');
@@ -21,19 +25,21 @@ var $parkAddressLine2 = document.querySelector('#park-address-line2');
 var $parkPhoneNumber = document.querySelector('#park-phone-number');
 var $parkEmail = document.querySelector('#park-email');
 
+var parkCode = null;
+
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
 function refreshSearchList() {
   var $li = document.querySelectorAll('.park-info-card');
-  // console.log($li);
   for (var j = 0; j < $li.length; j++) {
     $searchResult.removeChild($li[j]);
   }
 }
 
 function submitSearch(event) {
+  showSearchResult();
   refreshSearchList();
   event.preventDefault();
   var result = 'q=' + event.target.elements.q.value + '&';
@@ -41,29 +47,35 @@ function submitSearch(event) {
   xhr.open('GET', 'https://developer.nps.gov/api/v1/parks?limit=468&' + result + 'api_key=zfq2cSth1H6ynVcxdUiCUfdUdsTPyW6nusqU7OFY');
   xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
-    for (var i = 0; i < xhr.response.data.length; i++) {
-      var $parkInfoCard = document.createElement('li');
-      $parkInfoCard.setAttribute('class', 'column-third park-info-card');
+    if (xhr.response.data.length !== 0) {
+      $searchPlaceholder.className = 'search-placeholder hidden';
+      for (var i = 0; i < xhr.response.data.length; i++) {
+        var $parkInfoCard = document.createElement('li');
+        $parkInfoCard.setAttribute('class', 'column-third park-info-card');
 
-      var $parkInfoContentBox = document.createElement('div');
-      $parkInfoContentBox.setAttribute('class', 'park-info-content-box');
+        var $parkInfoContentBox = document.createElement('div');
+        $parkInfoContentBox.setAttribute('class', 'park-info-content-box');
 
-      var $parkH3 = document.createElement('h3');
-      $parkH3.textContent = xhr.response.data[i].fullName;
+        var $parkH3 = document.createElement('h3');
+        $parkH3.textContent = xhr.response.data[i].fullName;
 
-      var $parkImg = document.createElement('img');
-      $parkImg.setAttribute('src', xhr.response.data[i].images[0].url);
-      $parkImg.setAttribute('alt', xhr.response.data[i].images[0].altText);
+        var $parkImg = document.createElement('img');
+        $parkImg.className = 'park-image';
+        $parkImg.setAttribute('src', xhr.response.data[i].images[0].url);
+        $parkImg.setAttribute('alt', xhr.response.data[i].images[0].altText);
+        $parkImg.setAttribute('park-code', xhr.response.data[i].parkCode);
 
-      $parkInfoCard.appendChild($parkInfoContentBox);
-      $parkInfoContentBox.appendChild($parkH3);
-      $parkInfoContentBox.appendChild($parkImg);
+        $parkInfoCard.appendChild($parkInfoContentBox);
+        $parkInfoContentBox.appendChild($parkH3);
+        $parkInfoContentBox.appendChild($parkImg);
 
-      $searchResult.appendChild($parkInfoCard);
+        $searchResult.appendChild($parkInfoCard);
+      }
+    } else {
+      $searchPlaceholder.className = 'search-placeholder';
     }
   });
   xhr.send();
-  // console.log($searchResult);
 }
 
 function getParkData() {
@@ -91,17 +103,66 @@ function getParkData() {
   xhr.send();
 }
 
+function getParkDataByParkCode() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://developer.nps.gov/api/v1/parks?parkCode=' + parkCode + '&limit=1&api_key=zfq2cSth1H6ynVcxdUiCUfdUdsTPyW6nusqU7OFY');
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    $parkName.textContent = xhr.response.data[0].fullName;
+    $parkImages1.setAttribute('src', xhr.response.data[0].images[0].url);
+    $parkImages1.setAttribute('alt', xhr.response.data[0].images[0].altText);
+    $parkDescription.textContent = xhr.response.data[0].description;
+    $parkState.textContent = xhr.response.data[0].states;
+    $parkLink.textContent = xhr.response.data[0].fullName;
+    $parkLink.setAttribute('href', xhr.response.data[0].url);
+    $parkWeatherInfo.textContent = xhr.response.data[0].weatherInfo;
+    $cost.textContent = '$ ' + xhr.response.data[0].entranceFees[0].cost;
+    $costDescription.textContent = xhr.response.data[0].entranceFees[0].description;
+    $parkAddressLine1.textContent = xhr.response.data[0].addresses[0].line1;
+    $parkAddressLine2.textContent = xhr.response.data[0].addresses[0].city + ' ' + xhr.response.data[0].addresses[0].stateCode + ' ' + xhr.response.data[0].addresses[0].postalCode;
+    $parkPhoneNumber.textContent = 'Tel: ' + xhr.response.data[0].contacts.phoneNumbers[0].phoneNumber;
+    $parkEmail.textContent = xhr.response.data[0].contacts.emailAddresses[0].emailAddress;
+    $parkDetail.className = 'park-detail';
+    parkCode = null;
+  });
+  xhr.send();
+}
+
+$searchResult.addEventListener('click', function (event) {
+  if (event.target.className === 'park-image') {
+    parkCode = event.target.getAttribute('park-code');
+    getParkDataByParkCode();
+    $mobileSearch.className = 'row mobile-search hidden';
+    $homePage.className = 'home-page hidden';
+    $body.className = 'overlay';
+    $parkList.className = 'park-list hidden';
+    $mobileSearchBox.value = '';
+    $desktopSearchBox.value = '';
+  }
+});
+
+function showSearchResult(event) {
+  $parkList.className = '$parkList';
+  $homePage.className = 'home-page hidden';
+  $body.className = 'overlay';
+  $parkDetail.className = 'park-detail hidden';
+}
+
 function getRandomPark(event) {
   $mobileSearch.className = 'row mobile-search hidden';
   $homePage.className = 'home-page hidden';
   $body.className = 'overlay';
+  $parkList.className = 'park-list hidden';
   getParkData();
 }
 
 function backHome(event) {
+  $mobileSearchBox.value = '';
+  $desktopSearchBox.value = '';
   $mobileSearch.className = 'row mobile-search';
   $homePage.className = 'home-page';
   $parkDetail.className = 'park-detail hidden';
+  $parkList.className = 'park-list hidden';
   $body.removeAttribute('class');
 }
 
@@ -110,43 +171,3 @@ $backHome.addEventListener('click', backHome);
 
 $mobileSearchSubmit.addEventListener('submit', submitSearch);
 $desktopSearchSubmit.addEventListener('submit', submitSearch);
-
-// function renderSearch(event) {
-//   var $parkInfoCard = document.createElement('div');
-//   $parkInfoCard.setAttribute('class', 'column-third park-info-card');
-
-//   var $parkInfoContentBox = document.createElement('div');
-//   $parkInfoContentBox.setAttribute('class', 'park-info-content-box');
-
-//   var $parkH3 = document.createElement('h3');
-//   $parkH3.textContent =
-
-//   var $parkImg = document.createElement('img');
-//   $parkImg.setAttribute = ('src', '');
-//   $parkImg.setAttribute = ('alt', '');
-
-//   $parkInfoCard.appendChild($parkInfoContentBox);
-//   $parkInfoContentBox.appendChild($parkH3);
-//   $parkInfoContentBox.appendChild($parkImg);
-// }
-
-// for (var i = 0; i = xhr.response.data.length; i++) {
-//   var $parkInfoCard = document.createElement('div');
-//   $parkInfoCard.setAttribute('class', 'column-third park-info-card');
-
-//   var $parkInfoContentBox = document.createElement('div');
-//   $parkInfoContentBox.setAttribute('class', 'park-info-content-box');
-
-//   var $parkH3 = document.createElement('h3');
-//   $parkH3.textContent = xhr.response.data[i].fullName;
-
-//   var $parkImg = document.createElement('img');
-//   $parkImg.setAttribute = ('src', xhr.response.data[i].images[0].url);
-//   $parkImg.setAttribute = ('alt', xhr.response.data[i].images[0].altText);
-
-//   $parkInfoCard.appendChild($parkInfoContentBox);
-//   $parkInfoContentBox.appendChild($parkH3);
-//   $parkInfoContentBox.appendChild($parkImg);
-
-//   $searchResult.appendChild($parkInfoCard);
-// }
